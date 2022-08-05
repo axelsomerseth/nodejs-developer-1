813d2d3871ba97df4aa48b13ddf9176fe1471c6e
Axel Somerseth Córdova López <axelsomerseth@gmail.com>
2022-08-04 22:15:39 -0600

    basic cli working

--- cli/index.js
+++ cli/index.js
@@ -1,1 +1,12 @@
-console.log("This is a CLI tool.");
+const quotes = require("../data/quotes.json");
+const clic = require("cli-color");
+
+const randomIndex = Math.floor(Math.random() * quotes.length);
+const randomQuote = quotes[randomIndex];
+const symbol = clic.magenta;
+const quote = clic.yellow(randomQuote.quote);
+const author = clic.cyan(randomQuote.author);
+
+const output = `${symbol("«")}${quote}${symbol("»")} ${symbol("⸺")} ${author}`;
+
+console.log(output);
